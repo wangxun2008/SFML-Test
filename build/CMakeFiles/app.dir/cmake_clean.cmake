965a45fbb8261b5bbbf33acbb2bed75ecf3f7077
wangxun2008 <george_love_2008@163.com>
2025-07-06 09:29:43 +0800

    初次提交：SFML + CMake 图形窗口项目

---
+++ build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/src/main.cpp.obj"
+  "CMakeFiles/app.dir/src/main.cpp.obj.d"
+  "app.exe"
+  "app.exe.manifest"
+  "app.pdb"
+  "libapp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
